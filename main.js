df724d02b6c6bfce6dd46aaa11cad3dd86c51450
cody8764 <111713735+cody8764@users.noreply.github.com>
2023-11-02 20:04:41 -0400

    g

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function take_snapshot() {
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JSomHpSH0/',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JSomHpSH0/model.json',modelLoaded);
 
 function modelLoaded() {
     console.log("Model Loaded")
